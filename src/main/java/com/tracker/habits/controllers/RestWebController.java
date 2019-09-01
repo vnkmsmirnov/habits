4b1d15b8680c63161fdae2c01c619ddebf1d0b3e
Mikhail Smirnov <vnk.m.smirnov@gmail.com>
2019-09-01 19:06:23 +0300

    get mapping /add

--- src/main/java/com/tracker/habits/controllers/RestWebController.java
+++ src/main/java/com/tracker/habits/controllers/RestWebController.java
@@ -6,13 +6,11 @@ import com.tracker.habits.services.HabitsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/event")
@@ -31,9 +29,6 @@ public class RestWebController {
         String jsonMsg = null;
         try {
             List<Event> allHabits = habitsService.findAll();
-            for (Event e : allHabits) {
-                System.out.println(e.getType() + " " + e.getStart() + " " + e.getEnd());
-            }
             ObjectMapper mapper = new ObjectMapper();
             jsonMsg =  mapper.writerWithDefaultPrettyPrinter().writeValueAsString(allHabits);
         } catch (IOException ioex) {
@@ -43,19 +38,33 @@ public class RestWebController {
     }
 
     @GetMapping(value = "/add")
-    public String addEvents() {
+    public String addEvents(
+                            @RequestParam(value = "start", required = false) String start,
+                            @RequestParam(value = "end", required = false) String end,
+                            @RequestParam(value = "type", required = false) String type) {
+
         System.out.println("In method AddEvents");
-        Date date = new Date();
+
         String jsonMsg = null;
-        Event event = new Event();
-        event.setType("first event");
-        java.sql.Timestamp timestamp = new java.sql.Timestamp(date.getTime());
-        event.setStart(timestamp);
-        event.setEnd(timestamp);
+
+        System.out.println(start);
+        System.out.println(end);
+        System.out.println(type);
+
+//        String json = readJsonFromUrl(sURL);
+//        System.out.println(json);
 
 
-        System.out.println(event.getStart());
-        habitsService.saveOrUpdate(event);
+//        Date date = new Date();
+//        Event event = new Event();
+//        event.setType("first event");
+//        java.sql.Timestamp timestamp = new java.sql.Timestamp(date.getTime());
+//        event.setStart(timestamp);
+//        event.setEnd(timestamp);
+//        System.out.println(event.getStart());
+//        habitsService.saveOrUpdate(event);
+
         return "index";
     }
+
 }
